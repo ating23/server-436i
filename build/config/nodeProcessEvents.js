131030ec18712521ac5f6475626e0f13b9b1a178
Eduardo Garza <eduardo@garza.ca>
2020-06-13 01:01:51 -0700

    removed build

--- build/config/nodeProcessEvents.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.nodeProcessEvents = void 0;
-var nodeProcessEvents;
-(function (nodeProcessEvents) {
-    nodeProcessEvents["UNCAUGHT_EXCEPTION"] = "uncaughtException";
-    nodeProcessEvents["MESSAGE"] = "message";
-    // ...
-})(nodeProcessEvents = exports.nodeProcessEvents || (exports.nodeProcessEvents = {}));
