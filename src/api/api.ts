5fb904f005c527699a12d8ec81bcf03e12cda750
Eduardo Garza <eduardo@garza.ca>
2020-06-20 14:29:47 -0700

    removed /test from deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -18,6 +18,9 @@ const api = express()
 api.use(accountRoutes, verifyAuthorizationToken, accountRouter)
 api.use(authRoutes, authRouter)
 // api.use(sessionRoutes, sessionRouter)
-api.use(testRoutes, testRouter)
+
+if (process.env.NODE_ENV === "development") {
+  api.use(testRoutes, testRouter)
+}
 
 export default api
