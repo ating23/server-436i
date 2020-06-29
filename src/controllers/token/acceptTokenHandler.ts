9ce4478973bfea6b08dfa0aee55f1f3125e8c098
Eduardo Garza <eduardo@garza.ca>
2020-06-29 00:05:06 -0700

    spotify set

--- src/controllers/token/acceptTokenHandler.ts
+++ src/controllers/token/acceptTokenHandler.ts
@@ -2,6 +2,11 @@ import { Request, Response, NextFunction } from "express"
 import Logger from "../../errors/Logger"
 
 export default function acceptTokenHandler (req: Request, res: Response, next: NextFunction): void {
+  console.log()
+  console.log ("Hit /acceptTokenHandler")
+  console.log(req.headers)
+  console.log()
+  
   const { service, accessToken, expiresAt, refreshToken } = req.body
   Logger.Log ("service, accessToken, expiresAt, refreshToken")
   Logger.Log (service, accessToken, expiresAt, refreshToken)
